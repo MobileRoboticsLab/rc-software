c90926cb11200da5998c3572fb90bcea235b838a
Jonathan Boylan <jtylerboylan@outlook.com>
2023-09-07 23:03:07 -0400

    Finishes

---
+++ rviz_display.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Host Setup:
+# $ nano /etc/ssh/ssh_config
+# 'ForwardX11 yes'
+# $ xhost +
+
+# Start the Docker container
+docker run -it \
+    --rm \
+    --net host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e ROS_MASTER_URI=http://localhost:11311 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    osrf/ros:noetic-desktop-full \
+    rviz
--- setup_host.sh
+++ setup_host.sh
@@ -7,15 +7,15 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 apt-get update
 apt-get install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
 # Install latest version
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
